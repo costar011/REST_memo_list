fd13b1b2409e8dd47dce0ccff8b5cc0127c39958
costar011 <dnaer456@gmail.com>
2021-12-05 01:09:22 +0900

    memo

---
+++ db.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 1,
+    title: "첫번째",
+    createdAt: "2021-12-04 12:12:!2",
+    author: "방효진",
+  },
+  {
+    id: 2,
+    title: "2번째",
+    createdAt: "2021-12-04 12:11:!2",
+    author: "정예림",
+  },
+  {
+    id: 3,
+    title: "3번째",
+    createdAt: "2021-12-04 12:12:!7",
+    author: "오민형",
+  },
+  {
+    id: 4,
+    title: "4번째",
+    createdAt: "2021-12-04 12:12:!2",
+    author: "방효진",
+  },
+];
