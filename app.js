7d9c939dc3c4d34626cb857b63eb2404752ab147
costar011 <dnaer456@gmail.com>
2021-12-08 19:48:24 +0900

    desc

--- app.js
+++ app.js
@@ -7,16 +7,20 @@ const cors = require("cors");
 
 const memos = require("./db");
 
+// app 이라는 변수를 만들었다.
 const app = express();
-const PORT = 5000;
+const PORT = 5000; // PORT 는 5000
 
+// morgan을 combined 모드로 사용할거다.
 app.use(morgan("combined"));
 app.use(cors());
 
+// GET 방식
 app.get("/api/memo/list", (req, res, next) => {
   res.status(200).json(memos);
 });
 
+// 설정 끝난 후 Server Start
 app.listen(PORT, () => {
   console.log("5000 MEMO REST SERVER");
 });
